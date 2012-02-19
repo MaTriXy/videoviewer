f07406b13bd7c2c18e4757f4dd735680982ba198
Mark Whaite <whaity@gmail.com>
2012-02-19 12:13:20 +0000

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Videoviewer::Application.config.secret_token = 'a0fbc3e986d8cdc0cd192f6978ace86a68a7ad29bc8522fcc66ae117cb3d12675ed7ee3d39c612920ea1fa936f9181b75db5805356ca7340263bb955d76d84fc'
